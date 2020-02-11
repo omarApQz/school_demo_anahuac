28d132a19cb739492f0e004f5d60d222501aa39a
Brandon Vergara <leovergara.dar@gmail.com>
2020-02-10 23:44:48 -0600

    coding master  creating project

---
+++ src/main/groovy/com/nahuac/school/SchoolApplication.groovy
@@ -0,0 +1,13 @@
+package com.nahuac.school
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SchoolApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SchoolApplication, args)
+	}
+
+}
